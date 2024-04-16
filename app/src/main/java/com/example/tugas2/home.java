7dc89a9f88541e9c48f9b4426df02795c3a0f75d
muhammad reza al fatah <muhammadrezaalfatah09@gmail.com>
2024-04-16 21:28:27 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas2/home.java
@@ -0,0 +1,27 @@
+package com.example.tugas2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_landing);
+
+    }
+    public void auth (View view) {
+        Intent intent = new Intent(home.this,auth.class);
+        startActivity(intent );
+
+    }
+}
